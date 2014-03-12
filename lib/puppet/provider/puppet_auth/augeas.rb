ca545361607924a20b0aa3abdd9c31d66d4aeb67
Zack Smith <zack@puppetlabs.com>
2014-03-12 13:19:40 -0700

    Simple hack to get around bug
    Prior to this commit setting the target did not seem to work
    Putting this in place until I can track down the issue

--- lib/puppet/provider/puppet_auth/augeas.rb
+++ lib/puppet/provider/puppet_auth/augeas.rb
@@ -17,7 +17,7 @@ Puppet::Type.type(:puppet_auth).provide(:augeas) do
     "last deny"   => "path[count(allow)=0][last()]",
   }
 
-  default_file { '/etc/puppet/auth.conf' }
+  default_file { '/etc/puppetlabs/puppet/auth.conf' }
 
   lens { 'Puppet_Auth.lns' }
 
